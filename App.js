4f338a1cfaed07a25f344eace77de917eb4074f5
cruxbreaker <geekankit318@gmail.com>
2018-09-02 15:02:36 +0530

    Added Redux

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
+
+import ConfigureStore from './src/components/Store/config';
 
 import Login from './src/components/views/Login';
 import Home from './src/components/views/Home';
 import AddPost from './src/components/views/Admin/AddPost';
 
-Navigation.registerComponent("sellitApp.Login", ()  => Login);
-Navigation.registerComponent("sellitApp.Home", ()  => Home);
-Navigation.registerComponent("sellitApp.AddPost", ()  => AddPost);
+const store = ConfigureStore();
+
+Navigation.registerComponent(
+  "sellitApp.Login", 
+  ()  => 
+  Login,
+  store,
+  Provider
+);
+
+Navigation.registerComponent(
+  "sellitApp.Home", 
+  ()  => 
+  Home,
+  store,
+  Provider
+);
+
+Navigation.registerComponent(
+  "sellitApp.AddPost", 
+  ()  => 
+  AddPost,
+  store,
+  Provider
+);
 
 export default () => Navigation.startSingleScreenApp( {
   screen: {
---
+++ src/components/Store/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import User from './user_reducer';
+
+const rootReducer = combineReducers({
+    User
+});
+
+export default rootReducer;
