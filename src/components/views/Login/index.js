62b48f4d0c00c09f2c1512568f15942f8d6250fd
cruxbreaker <geekankit318@gmail.com>
2018-09-02 16:12:07 +0530

    Added animated Text(App Name)

--- src/components/views/Login/index.js
+++ src/components/views/Login/index.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 
 import LoadTabs from '../Tabs';
+import Logo from './logo';
 
 class Login extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Login</Text>
-        <Button 
-          title="go to home"
-          onPress={() => {
-            LoadTabs();
-          }}
-        />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Logo/>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  }
 });
 
 export default Login;
---
+++ src/components/views/Login/logo.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Animated, Easing } from 'react-native';
+
+class Logo extends Component {
+
+    state = {
+        sellAnim: new Animated.Value(0),
+        itAnim: new Animated.Value(0)
+    }
+
+    componentWillMount() {
+        Animated.sequence([
+            Animated.timing(this.state.sellAnim, {
+                toValue: 1,
+                duration: 1000,
+                easing: Easing.easeOutCubic
+            }),
+            Animated.timing(this.state.itAnim, {
+                toValue: 1,
+                duration: 500,
+                easing: Easing.easeOutCubic
+            })
+        ]).start(() => {
+            alert('Done');
+        })
+    }
+
+  render() {
+    return (
+      <View>
+        <View style={styles.logoStyles}>
+            <Animated.View
+                style={{
+                    opacity: this.state.sellAnim,
+                    top: this.state.sellAnim.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [100, 0]
+                    })
+                }}
+            >
+                <Text style={styles.sell}>Sell</Text>
+            </Animated.View>
+            <Animated.View
+                style={{
+                    opacity: this.state.itAnim 
+                }}
+            >
+                <Text style={styles.it}>It</Text>
+            </Animated.View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    logoStyles: {
+        marginTop: 50,
+        flex: 1,
+        flexDirection: 'row',
+        maxHeight: 100  
+    },
+    sell: {
+        fontSize: 40,
+        fontFamily: 'RobotoCondensed-Regular',
+        color: '#555555'
+    },
+    it: {
+        fontSize: 40,
+        fontFamily: 'RobotoCondensed-Regular',
+        color: '#00ADA9'
+    }
+});
+
+export default Logo;
